cd229b71b41b427af094bbd0c5cbc09f75fa992e
TangiLC <tangi@le-cadre.net>
2023-12-19 22:20:39 +0100

    test

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./index.js";
-
-test("renders", () => {
-	render(<App />);
-	const anything = screen.getByText("");
-	expect(anything).toBeInTheDocument();
-});
